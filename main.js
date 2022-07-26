3e84d2718ec03c489dcbabffec2a77f02e87a733
razvan chiriac <chiriac_razvan@ymail.com>
2022-07-27 00:58:04 +0300

    other try to fix add points and border + small refactoring

--- main.js
+++ main.js
@@ -9,20 +9,6 @@ const dataDescendentSortedByPoints = mockData.sort(
 
 console.log(dataDescendentSortedByPoints);
 
-let cards = "";
-
-//const addPointsButton = document.querySelector(".add-points");
-
-// function addPoints() {
-//   console.log("merge");
-
-//   console.log("addpoints");
-//   if (addPointsButton) {
-//     document.querySelector(".points").textContent = points + 1;
-//   }
-//   console.log("button pressed");
-// }
-
 function changeBorderColorMouseOver(currentColor) {
   // const border = document.querySelector(".border");
   // border.addEventListener("mouseenter", () => {
@@ -32,74 +18,93 @@ function changeBorderColorMouseOver(currentColor) {
   console.log("mouse over");
 }
 
+function mouseOver(hex) {
+  document.querySelector(
+    ".border"
+  ).style.border.style.cssText = `border-top: solid 2px  ${hex};
+  border-right: solid 2px  ${hex}`;
+}
+function mouseOut() {
+  document.querySelector(".border").style.borderRightColor = "black";
+}
+// onMouseOver="${() => mouseOver(hex)}"
+// onMouseOut="${() => mouseOut()}"
+
 // TODO: change border color
-for (let i = 0; i < 3; i++) {
-  let currentColor = dataDescendentSortedByPoints[i].hex;
+// for (let i = 0; i < dataDescendentSortedByPoints.length; i++)
 
-  let points = dataDescendentSortedByPoints[i].points;
+let cards = "";
 
-  cards += `
-  <article class="card">
+dataDescendentSortedByPoints.forEach(
+  (
+    { firstName, lastName, number, team, points, image, country, hex },
+    index
+  ) => {
+    cards += `
+
+    <article class="card">
   <div
-  class="border"
-  onMouseOver="${changeBorderColorMouseOver(currentColor)}"
-  onMouseOut="changeBorderColorMouseOut()"
+    class="border"
   >
-  </div>
     <div class="card-header">
-      <p class="rank">${i + 1}</p>
+      <p class="rank">${index + 1}</p>
       <span class="points-and-button">
-      <button type="button" class="add-points" >Add PTS</button>
-        <p class="points">${points}</p>
+        <button type="button" class="add-points">Add PTS</button>
+        <p class="points-text">${points}</p>
       </span>
     </div>
     <hr />
     <div class="card-body">
       <div class="container-name">
         <div class="name-and-line">
-          <span class="line-color" style="background:${currentColor};"></span>
+          <span class="line-color" style="background:${hex};"></span>
           <span class="name">
-            <p class="firstName">
-              ${dataDescendentSortedByPoints[i].firstName}
-            </p>
-            <p class="lastName">${dataDescendentSortedByPoints[i].lastName}</p>
+            <p class="firstName">${firstName}</p>
+            <p class="lastName">${lastName}</p>
           </span>
         </div>
-        <p class="country-flag">${dataDescendentSortedByPoints[i].country}</p>
+        <p class="country-flag">${country}</p>
       </div>
       <hr />
-      <p class="team-name">${dataDescendentSortedByPoints[i].team}</p>
+      <p class="team-name">${team}</p>
       <div class="photo-container">
         <div class="photo-background"></div>
-        <img
-          src="${dataDescendentSortedByPoints[i].image}"
-          alt="pilot's photo"
-          class="photo"
-        />
+        <img src="${image}" alt="pilot's photo" class="photo" />
 
-        <p class="number">${dataDescendentSortedByPoints[i].number}</p>
+        <p class="number">${number}</p>
       </div>
     </div>
+  </div>
 </article>
-            `;
 
-  if (i == 2) {
-    console.log(i);
-    const buttons = document.querySelectorAll(".add-points");
+  `;
 
-    buttons.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        console.log("pressed");
-        const points = btn.parentNode.children[1];
-        console.log(points);
-
-        points.textContent = +points.textContent + 1;
-      });
+    document.querySelector(".container").addEventListener("mouseenter", (e) => {
+      if (e.target.classList.contains("border")) {
+        console.log("border hover");
+        const border = document.querySelector(".border");
+        border.style.borderTopColor = hex;
+      }
     });
   }
-}
+  // const pointsText = document.getElementsByClassName("points");
+  // const button = document.querySelector("add-points");
+  // let pointsNr = parseInt(pointsText.innerText);
+  // button[index].addEventListener("click", () => {
+  //   pointsNr += 1;
+  //   pointsText.innerText = pointsNr;
+  // });
+);
 
-document.querySelector(".container").insertAdjacentHTML("beforeend", cards);
+const container = document.querySelector(".container");
+container.insertAdjacentHTML("beforeend", cards);
+
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("points-text")) {
+    console.log("pressed add points");
+    e.target.textContent += 1;
+  }
+});
 
 //Aici am incercat mai multe metode de a schimba culoarea la border
 // function changeBorderColorMouseOut() {
